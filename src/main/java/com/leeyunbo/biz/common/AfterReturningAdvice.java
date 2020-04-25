77dba9a9cc99c74c8e03a786835af6e9ef097d63
leeyunbo <servers1@naver.com>
2020-04-25 19:18:17 +0900

    [Spring AoP] Aop 에노테이션 설정 학습

--- src/main/java/com/leeyunbo/biz/common/AfterReturningAdvice.java
+++ src/main/java/com/leeyunbo/biz/common/AfterReturningAdvice.java
@@ -1,12 +1,23 @@
 package com.leeyunbo.biz.common;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.springframework.stereotype.Service;
 
 import com.leeyunbo.biz.user.UserVO;
 
-public class AfterReturning {
+@Service
+@Aspect
+public class AfterReturningAdvice {
+	@Pointcut("execution(* com.leeyunbo.biz..*Impl.get*(..))")
+	public void getPointcut() {}
+	
+	@AfterReturning(pointcut="getPointcut()", returning="returnObj")
 	public void afterLog(JoinPoint jp, Object returnObj) {
 		String method = jp.getSignature().getName();
+		
 		if(returnObj instanceof UserVO) { // 자식 -> 부모, 업캐스팅이 선행이 되었음
 			UserVO user = (UserVO) returnObj; // 따라서, 부모 -> 자식이되는 다운 캐스팅이 가능함
 			if(user.getRole().equals("Admin")) {
--- src/main/java/com/leeyunbo/biz/common/AfterThrowingAdvice.java
+++ src/main/java/com/leeyunbo/biz/common/AfterThrowingAdvice.java
@@ -1,8 +1,18 @@
 package com.leeyunbo.biz.common;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Service;
 
-public class AfterThrowing {
+@Service
+@Aspect
+public class AfterThrowingAdvice {
+	@Pointcut("execution(* com.leeyunbo.biz..*Impl.*(..))")
+	public void allPointcut() {}
+	
+	@AfterThrowing(pointcut="allPointcut()", throwing="exceptObj")
 	public void exceptionLog(JoinPoint jp, Exception exceptObj) {
 		String method = jp.getSignature().getName();
 		
